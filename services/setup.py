ae6eb6dd7f09908dd24e934899ef8925b81f3931
Taaseen Ali <taaseen.ali89@gmail.com>
2020-06-25 15:19:40 -0400

    refactor with docker

--- services/setup.py
+++
@@ -1,7 +0,0 @@
-import postgres
-
-print("===============================")
-print("Postgres Database Configuration")
-print("===============================\n")
-postgres.configure()
-postgres.create_tables(postgres.get_login())
