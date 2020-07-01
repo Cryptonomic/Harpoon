16bb43a08b1528d65711cc58ed2723c457d082d4
Taaseen Ali <taaseen.ali89@gmail.com>
2020-07-01 03:10:31 -0400

    CORS fix

--- ui/assets/networkConf.js
+++ ui/assets/networkConf.js
@@ -1,4 +1,4 @@
 const conseilServer = {"url": "<conseil_host>", "apiKey": "<api_key>"}
 const platform = 'tezos'
 const network = 'mainnet'
-const microseilServer = `localhost:8080/info`;
+const microseilServer = `http://localhost:8080/info`;
