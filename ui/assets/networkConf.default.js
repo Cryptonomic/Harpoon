6094756ad2a369a4b5eb1e6ee2cd4545975d0383
Taaseen Ali <taaseen.ali89@gmail.com>
2020-07-06 22:21:41 -0400

    added constants

--- ui/assets/networkConf.default.js
+++ ui/assets/networkConf.default.js
@@ -2,3 +2,13 @@ const conseilServer = {"url": "<conseil_host>", "apiKey": "<api_key>"}
 const platform = 'tezos'
 const network = 'mainnet'
 const microseilServer = `http://localhost:8080/info`;
+
+// Tezos constants
+const blocksPerCycleValues = {"mainnet" : 4096,
+			      "babylonnet" : 2048,
+			      "carthagenet" : 2048,
+			      "zeronet" : 128}
+const blocksPerCycle = blocksPerCycleValues[network]
+const tezPerRoll = 8000
+const CYCLES_PRESERVED = 5;
+const CYCLES_PENDING = 2
